f28ef91ab5dfebbf7382c3bde5b8030160e4cf83
Daniel <danielbnhz@gmail.com>
2025-04-19 22:00:04 -0700

    added more styling

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ import { gsap } from "gsap";
 
 
 
-gsap.from(".banner", { opacity: 0, duration: 1 }).fromto(".banner",{ opacity: 1, duration: 5   });
+gsap.from(".banner-with-text", { opacity: 0, duration: 1 })
+.fromto(".banner-with-text",{ opacity: 1, duration: 5   });
 
