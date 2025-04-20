2759864b99d35980f65a2db0e85b05d0eaaa8f0c
Daniel <danielbnhz@gmail.com>
2025-04-19 19:16:22 -0700

    installed vite, deleted old files, restarting the project basically. Only keeping the first commit for learning purposes.

--- app.js
+++
@@ -1,9 +0,0 @@
-import { gsap } from "gsap";
-    
-import { RoughEase, SlowMo } from "gsap/EasePack";
-    
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { TextPlugin } from "gsap/TextPlugin";
-
-
-gsap.registerPlugin(ScrollTrigger,TextPlugin,RoughEase,SlowMo);
