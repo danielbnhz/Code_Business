599d0f7645a4ac2f9c15f8b7c0b9c31cfe3c2423
Daniel <danielbnhz@gmail.com>
2025-04-19 21:53:57 -0700

    aimation of banner coded

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import { gsap } from "gsap";
 
-gsap.to(".banner", { rotation: 27, duration: 1 });
+
+
+gsap.from(".banner", { opacity: 0, duration: 1 }).fromto(".banner",{ opacity: 1, duration: 5   });
 
