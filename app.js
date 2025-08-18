6c9e911737c6464482ff9d5f0fe1fc20ea28d1a7
Daniel <danielbnhz@gmail.com>
2025-08-17 18:40:51 -0700

    fixed stagger animation

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 import { gsap } from "gsap";
-    
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 
 gsap.registerPlugin(ScrollTrigger);
 
-
-gsap.from(".banner-with-text", { opacity: 0, duration: 1 })
-.fromto(".banner-with-text",{ opacity: 1, duration: 5   });
-
-let intro = document.querySelector(".intro")
-
-gsap.to(intro, {
-    y: 55,
-    stagger: {
-        // wrap advanced options in an object
-        each: 0.4,
-        grid: 'auto',
-        repeat: -1
+document.addEventListener("DOMContentLoaded", () => {
+  // Banner animation
+    gsap.from(".banner-with-text", { opacity: 0, duration: 1 });
+  
+    gsap.from(".stagger-item", {
+    opacity: 0,
+    y: 20,
+    stagger: 0.3,
+    duration: 0.8,
+    scrollTrigger: {
+        trigger: ".intro",
+        start: "top 80%",
+        end: "bottom 20%",
+        toggleActions: "play none none reverse"
     }
+    });
+
+
+
 });
